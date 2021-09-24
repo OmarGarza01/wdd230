ae92277be7dc72086b0b93b8174f1b84435c4d39
OmarGarza01 <omarrlg@gmail.com>
2021-09-24 13:23:33 -0600

    remove extra folder

--- lesson2/lastmodified.js
+++
@@ -1,4 +0,0 @@
-//document.write("Last Updated: " + document.lastModified +"");
-
-let lastModified = new Date(document.lastModified);
-document.getElementById("lastmodified").innerHTML = lastModified;

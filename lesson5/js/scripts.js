616e43a6775d2baf428e677d03f6ca7515ed5da3
OmarGarza01 <omarrlg@gmail.com>
2021-10-15 13:57:12 -0600

    comit

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,10 +1,27 @@
-function toggleMenu(){
-    
-    document.getElementById("primaryNav").classList.toggle("hide");
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-
 const event = new Date();
-const options = { weekday: 'long', day: 'numeric', month: 'long',  year: 'numeric'};
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
 const date = event.toLocaleDateString(undefined, options);
 document.getElementById("currentDate").innerHTML = date;
+
+// close banner
+document.querySelector(".banner_close").addEventListener("click", function () {
+  this.closest(".banner").remove();
+});
+
+// load banner on friday
+function isFriday() {
+  let today = new Date();
+  let day = today.getDay();
+  if (day != 5) {
+    this.closest(".banner").remove();
+  }
+}

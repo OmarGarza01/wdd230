2b3eea82e39b7895e13823f5964ffd857baa5f2b
OmarGarza01 <omarrlg@gmail.com>
2021-10-21 17:50:23 -0600

    comit lesson 6

---
+++ lesson6/js/scripts.js
@@ -0,0 +1,30 @@
+// Menu
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+// get current date
+const event = new Date();
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+const date = event.toLocaleDateString(undefined, options);
+
+
+// close banner
+document.querySelector(".banner_close").addEventListener("click", function () {
+  this.closest(".banner").remove();
+});
+
+
+// load banner on friday
+var today = new Date();
+if(today.getDay() != 5) {
+  const friday = document.querySelector("#friday")
+  friday.remove()
+}
+
+

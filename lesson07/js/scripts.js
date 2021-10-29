d0e61e3fe501bdfd6754cd108ae3cec57842053b
OmarGarza01 <omarrlg@gmail.com>
2021-10-28 19:15:19 -0600

    comit

--- lesson07/js/scripts.js
+++ lesson07/js/scripts.js
@@ -13,19 +13,3 @@ const options = {
 };
 const date = event.toLocaleDateString(undefined, options);
 document.getElementById("currentDate").innerHTML = date;
-
-
-// close banner
-document.querySelector(".banner_close").addEventListener("click", function () {
-  this.closest(".banner").remove();
-});
-
-
-// load banner on friday
-var today = new Date();
-if(today.getDay() != 5) {
-  const friday = document.querySelector("#friday")
-  friday.remove()
-}
-
-
---
+++ lesson07/js/visits.js
@@ -0,0 +1,17 @@
+
+
+/*
+   Using local storage, display the amount of time in days 
+   (rounded to a whole number) between user visits to this
+   Gallery page by the user's agent (browser). You may elect to display
+    this information where you deem fit on the page.
+*/
+
+
+localStorage.setItem('name','bob')
+localStorage.removeItem('name')
+
+
+sessionStorage.setItem('name','omar')
+sessionStorage.removeItem('name')
+

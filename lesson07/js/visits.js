cc38ace12a57578608e786457b18d3a0cd3016f8
OmarGarza01 <omarrlg@gmail.com>
2021-10-28 19:40:59 -0600

    comit WS API

--- lesson07/js/visits.js
+++ lesson07/js/visits.js
@@ -8,10 +8,23 @@
 */
 
 
-localStorage.setItem('name','bob')
-localStorage.removeItem('name')
+let event1 = new Date()
+let options1 = {
+    day: "numeric",
+  };
+let date1 = event1.toLocaleDateString(undefined, options1);
+localStorage.setItem('date',date1)
 
 
-sessionStorage.setItem('name','omar')
-sessionStorage.removeItem('name')
+
+sessionStorage.setItem('date',date1)
+
+
+let n1 = localStorage.getItem('date')
+let n2 = sessionStorage.getItem('date')
+let dif = n1-n2;
+
+document.getElementById('visit').innerHTML = "It has been " + dif + " days since you were last here" ;
+
+
 

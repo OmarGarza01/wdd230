5900f91c3ba5bc08d39849ce8525e6fc5a63bcb1
OmarGarza01 <omarrlg@gmail.com>
2021-11-04 17:25:42 -0600

    comit

---
+++ lesson08/js/windchill.js
@@ -0,0 +1,18 @@
+// GET WINDCHILL
+let regExp = /\d+/;
+
+let string = document.getElementById("speed").textContent;
+let s = string.match(regExp);
+
+let string2 = document.getElementById("temp").textContent;
+let t = string2.match(regExp);
+
+let f =
+  35.75 +
+  0.6215 * t -
+  35.75 * Math.pow(s, 0.16) +
+  0.4275 * t * Math.pow(s, 0.16);
+
+if (t <= 50 && s > 3)
+  document.getElementById("windchill").innerHTML = Math.round(f) + " °F";
+else document.getElementById("windchill").innerHTML = "not applicable";

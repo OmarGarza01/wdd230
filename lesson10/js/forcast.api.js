1c8c8c40c1839fd88d8f65546302b82b4f724da0
OmarGarza01 <omarrlg@gmail.com>
2021-11-18 14:34:17 -0700

    comit

---
+++ lesson10/js/forcast.api.js
@@ -0,0 +1,19 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=a21468c147a898bf5b43fd9b2448580d';
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+   
+    console.log(jsObject);
+    const noon = jsObject.list.filter(x => x.dt_txt.includes('12:00:00'));
+    console.log(noon);
+
+    const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let day = 0;
+    noon.forEach(forecast => {
+        let thedate = new Date(forecast.dt_txt);
+        document.querySelector(`#day${day + 1}`).textContent = weekdays[thedate.getDay()];
+        document.querySelector(`#for${day + 1}`).textContent = forecast.main.temp;
+        day++;
+    })
+
+});
---
+++ lesson100/js/weatherapi.js
@@ -0,0 +1,20 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=a21468c147a898bf5b43fd9b2448580d';
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    document.getElementById('temp').textContent = jsObject.main.temp.toFixed(0);
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('windspeed').textContent = jsObject.wind.speed.toFixed(1);
+    document.getElementById('windchillapi').textContent = jsObject.main.feels_like.toFixed(0);
+    document.getElementById('currently').textContent = jsObject.weather[0].description;
+
+
+    
+
+    
+
+
+
+  });

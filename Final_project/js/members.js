d65aad20e53691b85827ce593ae891dd1a9ab1d3
OmarGarza01 <omarrlg@gmail.com>
2021-12-13 15:38:32 -0700

    comit

--- Final_project/js/members.js
+++ Final_project/js/members.js
@@ -10,15 +10,18 @@ fetch(requestURL)
         let member = document.createElement('section');
         let h2 = document.createElement('h2');
         let image = document.createElement('img');
+        let p = document.createElement('p');
        
 
         h2.textContent = members[i].name;
         image.setAttribute('src', members[i].photo);
+        p.textContent = members[i].address;
        
 
 
         member.appendChild(h2);
         member.appendChild(image);
+        member.appendChild(p);
         
 
 

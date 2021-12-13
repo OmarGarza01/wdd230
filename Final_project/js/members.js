d1b736354a227f0340e995f8f703f11fa17c1ba4
OmarGarza01 <omarrlg@gmail.com>
2021-12-13 15:06:58 -0700

    commit

--- Final_project/js/members.js
+++ Final_project/js/members.js
@@ -1,36 +1,28 @@
-requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+requestURL = 'https://raw.githubusercontent.com/OmarGarza01/wdd230/master/Final_project/json/members.json';
 
 fetch(requestURL)
 .then(function(response) {
     return response.json();
 })
 .then(function(jsonObject){
-    const towns = jsonObject['towns'];
-    for (let i = 0; i < towns.length; i++ ) {
-        let town = document.createElement('section');
+    const members = jsonObject['members'];
+    for (let i = 0; i < members.length; i++ ) {
+        let member = document.createElement('section');
         let h2 = document.createElement('h2');
-        let motto = document.createElement('q');
-        let year = document.createElement('p');
-        let pop = document.createElement('p');
-        let rain = document.createElement('p');
         let image = document.createElement('img');
-         
-        h2.textContent = towns[i].name;
-        motto.textContent = towns[i].motto;
-        year.textContent = 'Year Founded: ' + towns[i].yearFounded;
-        pop.textContent = 'Population: ' + towns[i].currentPopulation;
-        rain.textContent = 'Annual Rain Fall: '+ towns[i].averageRainfall;
-        image.setAttribute('src', towns[i].photo);
+        let web = document.createElement('p');
 
-        town.appendChild(h2);
-        town.appendChild(motto);
-        town.appendChild(year);
-        town.appendChild(pop);
-        town.appendChild(rain);
-        town.appendChild(image);
+        h2.textContent = members[i].name;
+        image.setAttribute('src', members[i].photo);
+        web.textContent = members[i].weblink;
 
 
-        document.querySelector('div.towns').appendChild(town);
+        member.appendChild(h2);
+        member.appendChild(image);
+        member.appendChild(web);
+
+
+        document.querySelector('div.members').appendChild(member);
 
     }
 
